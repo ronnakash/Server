15ed2cc9c38c5ae0bcda73620f2a314869ebe251
ronnakash <ronnakash@gmail.com>
2021-11-05 18:48:43 +0200

    Added premissions field to User schema added permissions field to User Schema. also updated README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tsServer
+
+Typescript backend WebApp for posting, getting and editing notes.
+You must register your user in order to post your notes.
--- src/models/user.ts
+++ src/models/user.ts
@@ -4,7 +4,8 @@ import IUser from '../interfaces/user';
 const UserSchema: Schema = new Schema(
     {
         username: { type: String, required: true, unique: true },
-        password: { type: String, required: true }
+        password: { type: String, required: true },
+        Permissions: { type: String, required: false}
     },
     {
         timestamps: true
