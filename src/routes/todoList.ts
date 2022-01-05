32c5eb73d5466d26c09eabef547ac0a6de781e2b
ronnakash <ronnakash@gmail.com>
2022-01-05 02:30:00 +0200

    added todo lists route

---
+++ src/routes/todoList.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import controller from '../controllers/todoLists';
+import handler from '../middleware/responseHandler';
+
+
+const router = express.Router();
+
+
+
+/** create */
+router.post('/post/list', controller.createList);
+router.post('/post/createLists', controller.createLists);
+/** read */
+router.get('/Admin/get/allLists' ,controller.getAllLists);
+router.get('/get/myLists', controller.getMyLists);
+/** update */
+router.put('/put/updateList', controller.updateList);
+/** delete */
+router.delete('/delete/deleteList', controller.deleteListById);
+router.delete('/delete/allUsersLists' ,controller.deleteAllUsersLists);
+
+
+router.use(handler);
+
+
+
+export default { router };
